6ced7550bf7f00730d7c2b2ff5f081b040384c3b
olya <olyaionova_98@mail.ru>
2018-02-25 21:32:26 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var stack=[];
+  str=str.split('');
+  if (str.length%2) return false;
+  str.forEach((bracket)=>{
+   bracketsConfig.forEach((c)=>{
+     
+     if (bracket==c[0] && bracket!=c[1]) {
+      stack.push(bracket);
+    } else if (bracket==c[0] && bracket==c[1]) {
+
+        if (stack[stack.length-1]==bracket) {
+        stack.pop();
+        } else {stack.push(bracket);}
+
+
+      } else if (stack[stack.length-1]==c[0] && bracket==c[1]){
+        stack.pop();
+      } else {return false}; 
+      
+  });
+  
+  });
+  return stack.length==0;
 }
